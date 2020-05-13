907bcf7ca9529950d8628105c1f0a7adbbfdaaad
faha-berdiev <madysoftware@gmail.com>
2020-05-13 17:16:20 +0300

    Вторая задача

--- Day 21/Program.cs
+++ Day 21/Program.cs
@@ -9,6 +9,7 @@ namespace Day_21
         static void Main(string[] args)
         {
             FirstTask();
+            SecondTask();
 
             Console.Read();
         }
@@ -17,6 +18,7 @@ namespace Day_21
         {
             string lineSeperator = new string('*', 25);
             Console.WriteLine("Task 1...");
+
             string oldStr = "hello";
             string newStr = new String(oldStr.Select(c => c == 'a' ? '1' : c)
                 .Select(c => c == 'e' ? '2' : c)
@@ -24,6 +26,23 @@ namespace Day_21
                 .Select(c => c == 'o' ? '4' : c)
                 .Select(c => c == 'u' ? '5' : c).ToArray());
             Console.WriteLine(oldStr + " => " + newStr);
+
+            Console.WriteLine(lineSeperator);
+        }
+        static void SecondTask()
+        {
+            string lineSeperator = new string('*', 25);
+            Console.WriteLine("Task 2...");
+
+            string oldStr = "h3 th2r2";
+            string newStr = new String(oldStr.Select(c => c == '1' ? 'a' : c)
+                .Select(c => c == '2' ? 'e' : c)
+                .Select(c => c == '3' ? 'i' : c)
+                .Select(c => c == '4' ? 'o' : c)
+                .Select(c => c == '5' ? 'u' : c).ToArray());
+            Console.WriteLine(oldStr + " => " + newStr);
+
+            Console.WriteLine(lineSeperator);
         }
     }
 }
