df8d18d034d7d9f465dbdcc9d70b5b584ee7f2e2
faha-berdiev <madysoftware@gmail.com>
2020-05-13 16:59:19 +0300

    Начало решения задачи

---
+++ Day 21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstTask();
+        }
+
+        static void FirstTask()
+        {
+            string lineSeperator = new string('*', 25);
+            Console.WriteLine("Task 1...");
+            string inputedStr = "hello";
+            Console.WriteLine();
+        }
+    }
+}
